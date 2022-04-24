32fd4245206989602bc81ff54459648ac28f3dd7
mdsumonali372 <mdsumonali026@gmail.com>
2022-04-24 22:43:57 +0600

    added

---
+++ photoweb/resources/views/header.blade.php
@@ -0,0 +1,3 @@
+    <link rel="stylesheet" href="{{asset('fontend/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('fontend/fontawesome/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{asset('fontend/css/templatemo-style.css')}}">
